41d435e2c1b367afbc93eff1feaccbaf6e1bcca1
zhanglc <122490502@qq.com>
2018-08-30 15:18:21 +0800

    first commit

---
+++ serialApp/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace serialApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxPort = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxBaud = new System.Windows.Forms.ComboBox();
+            this.buttonOpenClosePort = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.buttonAddFile = new System.Windows.Forms.Button();
+            this.buttonClearList = new System.Windows.Forms.Button();
+            this.listViewDisplayFileList = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.toolTipFileConfigButtonTips = new System.Windows.Forms.ToolTip(this.components);
+            this.buttonAddDir = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxFileProtocol = new System.Windows.Forms.ComboBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonOpenClosePort);
+            this.groupBox1.Controls.Add(this.comboBoxBaud);
+            this.groupBox1.Controls.Add(this.comboBoxPort);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.Location = new System.Drawing.Point(22, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(293, 247);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "串口配置";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(27, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "端口";
+            // 
+            // comboBoxPort
+            // 
+            this.comboBoxPort.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBoxPort.FormattingEnabled = true;
+            this.comboBoxPort.Location = new System.Drawing.Point(120, 45);
+            this.comboBoxPort.Name = "comboBoxPort";
+            this.comboBoxPort.Size = new System.Drawing.Size(127, 36);
+            this.comboBoxPort.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(27, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 31);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "波特率";
+            // 
+            // comboBoxBaud
+            // 
+            this.comboBoxBaud.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBoxBaud.FormattingEnabled = true;
+            this.comboBoxBaud.Location = new System.Drawing.Point(120, 105);
+            this.comboBoxBaud.Name = "comboBoxBaud";
+            this.comboBoxBaud.Size = new System.Drawing.Size(127, 36);
+            this.comboBoxBaud.TabIndex = 2;
+            // 
+            // buttonOpenClosePort
+            // 
+            this.buttonOpenClosePort.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonOpenClosePort.Location = new System.Drawing.Point(30, 166);
+            this.buttonOpenClosePort.Name = "buttonOpenClosePort";
+            this.buttonOpenClosePort.Size = new System.Drawing.Size(217, 41);
+            this.buttonOpenClosePort.TabIndex = 3;
+            this.buttonOpenClosePort.Text = "打开串口";
+            this.buttonOpenClosePort.UseVisualStyleBackColor = true;
+            this.buttonOpenClosePort.Click += new System.EventHandler(this.buttonOpenCloseSerial_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.buttonRemove);
+            this.groupBox2.Controls.Add(this.buttonAddDir);
+            this.groupBox2.Controls.Add(this.listViewDisplayFileList);
+            this.groupBox2.Controls.Add(this.buttonClearList);
+            this.groupBox2.Controls.Add(this.buttonAddFile);
+            this.groupBox2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox2.Location = new System.Drawing.Point(335, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(852, 555);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "文件选择配置";
+            // 
+            // buttonAddFile
+            // 
+            this.buttonAddFile.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonAddFile.Location = new System.Drawing.Point(18, 39);
+            this.buttonAddFile.Name = "buttonAddFile";
+            this.buttonAddFile.Size = new System.Drawing.Size(157, 41);
+            this.buttonAddFile.TabIndex = 4;
+            this.buttonAddFile.Text = "新增文件";
+            this.toolTipFileConfigButtonTips.SetToolTip(this.buttonAddFile, "向文件列表添加文件");
+            this.buttonAddFile.UseVisualStyleBackColor = true;
+            this.buttonAddFile.Click += new System.EventHandler(this.buttonAddFile_Click);
+            // 
+            // buttonClearList
+            // 
+            this.buttonClearList.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonClearList.Location = new System.Drawing.Point(555, 40);
+            this.buttonClearList.Name = "buttonClearList";
+            this.buttonClearList.Size = new System.Drawing.Size(157, 41);
+            this.buttonClearList.TabIndex = 5;
+            this.buttonClearList.Text = "清空列表";
+            this.toolTipFileConfigButtonTips.SetToolTip(this.buttonClearList, "清空文件列表");
+            this.buttonClearList.UseVisualStyleBackColor = true;
+            this.buttonClearList.Click += new System.EventHandler(this.buttonClearList_Click);
+            // 
+            // listViewDisplayFileList
+            // 
+            this.listViewDisplayFileList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listViewDisplayFileList.FullRowSelect = true;
+            this.listViewDisplayFileList.GridLines = true;
+            this.listViewDisplayFileList.Location = new System.Drawing.Point(17, 89);
+            this.listViewDisplayFileList.Name = "listViewDisplayFileList";
+            this.listViewDisplayFileList.Size = new System.Drawing.Size(813, 460);
+            this.listViewDisplayFileList.TabIndex = 6;
+            this.listViewDisplayFileList.UseCompatibleStateImageBehavior = false;
+            this.listViewDisplayFileList.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "文件名";
+            this.columnHeader1.Width = 153;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "文件大小(Bytes)";
+            this.columnHeader2.Width = 242;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "创建时间";
+            this.columnHeader3.Width = 455;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.comboBoxFileProtocol);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox3.Location = new System.Drawing.Point(22, 277);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(293, 290);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "文件处理";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(240, 46);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "开始传送";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(29, 178);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(240, 46);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "取消传输";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // toolTipFileConfigButtonTips
+            // 
+            this.toolTipFileConfigButtonTips.Tag = "";
+            // 
+            // buttonAddDir
+            // 
+            this.buttonAddDir.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonAddDir.Location = new System.Drawing.Point(196, 39);
+            this.buttonAddDir.Name = "buttonAddDir";
+            this.buttonAddDir.Size = new System.Drawing.Size(157, 41);
+            this.buttonAddDir.TabIndex = 7;
+            this.buttonAddDir.Text = "新增目录";
+            this.toolTipFileConfigButtonTips.SetToolTip(this.buttonAddDir, "向文件列表添加目录中包含的文件");
+            this.buttonAddDir.UseVisualStyleBackColor = true;
+            this.buttonAddDir.Click += new System.EventHandler(this.buttonAddDir_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 31);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "传输协议";
+            // 
+            // comboBoxFileProtocol
+            // 
+            this.comboBoxFileProtocol.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBoxFileProtocol.FormattingEnabled = true;
+            this.comboBoxFileProtocol.Items.AddRange(new object[] {
+            "Ymodem"});
+            this.comboBoxFileProtocol.Location = new System.Drawing.Point(138, 50);
+            this.comboBoxFileProtocol.Name = "comboBoxFileProtocol";
+            this.comboBoxFileProtocol.Size = new System.Drawing.Size(131, 36);
+            this.comboBoxFileProtocol.TabIndex = 4;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonRemove.Location = new System.Drawing.Point(376, 38);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(157, 41);
+            this.buttonRemove.TabIndex = 8;
+            this.buttonRemove.Text = "删除";
+            this.toolTipFileConfigButtonTips.SetToolTip(this.buttonRemove, "从文件列表中删除选择的行");
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1206, 583);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonOpenClosePort;
+        private System.Windows.Forms.ComboBox comboBoxBaud;
+        private System.Windows.Forms.ComboBox comboBoxPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView listViewDisplayFileList;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button buttonClearList;
+        private System.Windows.Forms.Button buttonAddFile;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolTip toolTipFileConfigButtonTips;
+        private System.Windows.Forms.Button buttonAddDir;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBoxFileProtocol;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button buttonRemove;
+    }
+}
+
---
+++ serialApp/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace serialApp
+{
+    public partial class Form1 : Form
+    {
+        #region 软件版本号
+        public const string _softwareVersion = "V1.0.0";
+        public const string _softwareName = "文件测试工具";
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           this.Text = _softwareName + _softwareVersion;
+
+            //init port combox 
+            string[] PortNames = SerialPort.GetPortNames();    //获取本机串口名称，存入PortNames数组中
+
+            if (PortNames.Count() <= 0)
+            {
+                MessageBox.Show("no ports available");
+
+            }
+            else
+            {
+                for (int i = 0; i < PortNames.Count(); i++)
+                {
+                    comboBoxPort.Items.Add(PortNames[i]);   //将数组内容加载到comboBox控件中
+                }
+
+                comboBoxPort.SelectedIndex = 0;
+            }
+
+            //init badurate combox
+            int[] baudrate_list = new int[5] { 115200, 19200, 38400, 9600, 57600 };
+            for (int i = 0; i < baudrate_list.Length; i++)
+            {
+                comboBoxBaud.Items.Add(baudrate_list[i].ToString());
+            }
+            comboBoxBaud.SelectedIndex = 0;
+
+
+            //init file protocl combox
+            comboBoxFileProtocol.SelectedIndex = 0;
+        }
+
+        private void buttonOpenCloseSerial_Click(object sender, EventArgs e)
+        {
+            if (buttonOpenClosePort.Text == "打开串口")
+            {
+                buttonOpenClosePort.Text = "关闭串口";
+                serialPort1.PortName = comboBoxPort.SelectedItem.ToString();
+                serialPort1.BaudRate = int.Parse(comboBoxBaud.SelectedItem.ToString());
+                try
+                {
+                    serialPort1.Open();
+                }
+                catch
+                {
+                    MessageBox.Show("串口打开失败");
+
+                }
+                MessageBox.Show("串口打开成功");
+
+                Console.WriteLine("Baudrate:{0},Port:{1}", serialPort1.BaudRate, serialPort1.PortName);
+            }
+            else
+            {
+                buttonOpenClosePort.Text = "打开串口";
+                try
+                {
+                    serialPort1.Close();
+                }
+                catch
+                {
+                    MessageBox.Show("串口关闭失败");
+
+                }
+                MessageBox.Show("串口关闭成功");
+
+            }
+        }
+
+        private void buttonAddFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "All files (*.*)|*.*";  //name|reg|name|reg
+            openFileDialog1.Title = "Choose a File";
+            openFileDialog1.Multiselect = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //遍历文件
+                FileInfo info;
+                ListViewItem item;
+                foreach (string filename in openFileDialog1.FileNames)
+                {
+                    info = new FileInfo(filename);
+                    string[] item_string = new string[3] { info.Name, info.Length.ToString() ,info.CreationTime.ToString("yyyy/MM/dd HH:mm:ss") };
+                    item = new ListViewItem(item_string);
+                    listViewDisplayFileList.Items.Add(item);
+                }
+            }
+        }
+
+        private void buttonAddDir_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string path = folderBrowserDialog1.SelectedPath;
+                Console.WriteLine("choose path = {0}"+path);
+
+                //遍历目录
+                DirectoryInfo directory = new DirectoryInfo(path);
+                ListViewItem item;
+                foreach (FileInfo file in directory.GetFiles())
+                {
+                    string[] item_string = new string[3] { file.Name, file.Length.ToString(), file.CreationTime.ToString("yyyy/MM/dd HH: mm:ss")};
+                    item = new ListViewItem(item_string);
+                    listViewDisplayFileList.Items.Add(item);
+                }
+            }
+        }
+
+        private void buttonClearList_Click(object sender, EventArgs e)
+        {
+            listViewDisplayFileList.Items.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //获取选定项目,调试代码
+            foreach (ListViewItem item in listViewDisplayFileList.SelectedItems)
+            {
+                string info = string.Empty;
+                for (int i=0;i< item.SubItems.Count;i++)
+                {
+                    info += " ";
+                    info +=item.SubItems[i].Text;
+                }
+                Console.WriteLine(info);
+            }
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            //获取选定项目
+            foreach (ListViewItem item in listViewDisplayFileList.SelectedItems)
+            {
+                item.Remove();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //修改选定项目,调试代码
+            foreach (ListViewItem item in listViewDisplayFileList.SelectedItems)
+            {
+                item.SubItems[0].Text = "modify";
+            }
+        }
+    }
+}
